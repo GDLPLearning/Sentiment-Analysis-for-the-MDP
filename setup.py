08c3b10af1542736d29ddb2e23f70fe5fc076fe2
GDLPLearning <gdlplearning@gmail.com>
2022-05-04 12:20:29 -0500

    Create project repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project repository - 'Sentiment Analysis in Twitter for the Medellin Development Plan' by group 247 of the 6th cohort of DS4A Colombia.',
+    author='Arteaga Juan, Caro Wilson, Quintero Jonathan, Lambis Erick, Luna German, Sosa Sergio, Vergara Gustavo ',
+    license='BSD-3',
+)
