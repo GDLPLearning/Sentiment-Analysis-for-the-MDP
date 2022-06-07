d4f64a32f1630c4f00dd9daf9e4e105bb69410b9
GDLPLearning <74005887+GDLPLearning@users.noreply.github.com>
2022-06-07 18:30:40 -0500

    Delete keys.py

--- src/data/keys.py
+++
@@ -1,6 +0,0 @@
-# keys.py
-"""Academic Research Access Application"""
-consumer_key = "WOqCjUlKHMwAe4iFztGqocZmc"
-consumer_secret = "g8diatpLrlKjEk8ZHl5oGPeGL9oxeLo8mEE6MrDuDTsD0uWBUd"
-access_token = "172106314-fWXki4XWQg0TMmHIJOwFZfBDKepXIA3r11wvaVki"
-access_token_secret = "90wFvOe2envsJHbJyeQXenW2rB0TUbBiAaYwqoJmRjGA3"
