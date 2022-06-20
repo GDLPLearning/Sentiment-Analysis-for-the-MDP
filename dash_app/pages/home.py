01143b01c8528e8ad5deea802a1faa323fad6243
gvergaraa77 <gustavovergara238@gmail.com>
2022-06-20 18:45:08 -0500

    Update

--- dash_app/pages/home.py
+++ dash_app/pages/home.py
@@ -5,4 +5,9 @@ from dash import html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
-layout =0
+layout =html.Div([
+    html.Div([
+        html.H1(children='OVERVIEW'),
+        html.Img(src='/assets/images/colombia-2722716_1920.jpg', style={'height': '700px', 'width': '700px'}),
+    ])
+])
