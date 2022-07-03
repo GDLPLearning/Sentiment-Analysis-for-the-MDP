897f7224dacad31a27265119fc0999d8fdac13e2
GDLPLearning <gdlplearning@gmail.com>
2022-07-03 13:21:44 -0500

    update

--- dash_app/pages/model.py
+++ dash_app/pages/model.py
@@ -12,4 +12,17 @@ layout = html.Div([
         html.H2(''),
         ], style={'textAlign': 'center'}),
 
+############### Tweet text preprocessing #################
+
+    dbc.CardHeader(html.H3('Tweet text preprocessing')),
+    html.P('Explicacion de esta sección'),
+    html.Br(),
+    dbc.Label('Tweet text:'),
+    dcc.Textarea(
+        id='textarea-example',
+        placeholder='Enter the text of the tweet',
+        style={'width': '100%', 'height': 100}),
+    html.Div([
+        dbc.Button("Generate Graphs", outline=True, color="primary", className="me-1", size="sm", id="button"),
+        ],  className="d-grid gap-2 col-6 mx-auto"),  
     ])
