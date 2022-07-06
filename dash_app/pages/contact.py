9c8b5e6a88bd97533e310f3891e40a81c34cdb22
gvergaraa77 <gustavovergara238@gmail.com>
2022-07-06 16:22:27 -0500

    Update

--- dash_app/pages/contact.py
+++ dash_app/pages/contact.py
@@ -1,4 +1,5 @@
 # import libraries
+from turtle import st
 import dash
 from dash import dcc
 from dash import html
@@ -10,6 +11,7 @@ style_div={
     'border':'#5BC0BE 0.03rem solid ',
     'border-radius':'1rem',
     'padding':'10px',
+    'height':'400px',
 }
 
 
@@ -24,40 +26,127 @@ layout=html.Div([
     dbc.Row([
         dbc.Col([
             html.Div([
-                html.H4('Gustavo Vergara', style={'textAlign': 'center'}),
-                html.Img(src='assets/profile/gv.png', style={'width': '45%', 'height': '45%', 'border-radius': '50%', 'margin-left': '25%'}),
-                html.Ul([
-                    html.Li('Matematician, Universidad Del Norte'),
-                    html.Li('Professor of Mathematics, Universidad Del Norte'),
-                    html.Li([
-                        html.Img(src='https://api.iconify.design/arcticons/huawei-email.svg', style={'width': '20px', 'height': '20px'}),
-                        ' gustavovergara238@gmail.com']),
-                    html.Li([
-                        html.Img(src="https://api.iconify.design/logos/linkedin-icon.svg", style={'width': '5%', 'height': '5%'}),  
-                        ' ',                  
-                        html.A('Gustavo Vergara', href='https://www.linkedin.com/in/gustavo-antonio-vergara-rolong-22b15a144/', style={'textDecoration': 'none'},target='_blank'),
+                dbc.CardHeader(html.H4('Germán Luna', style={'textAlign': 'center'})),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([]),
+                    dbc.Col([
+                        html.Div([
+                            html.Img(src="assets/profile/gv.png",style={'width': '150px', 'height': '150px','border-radius':'50%'}),    
+                        ], style={'textAlign': 'center'}),
                     ]),
-                    html.Li([
-                        html.Img(src='https://api.iconify.design/ant-design/github-filled.svg', style={'width': '5%', 'height': '5%'}),
-                        ' ',                      
-                        html.A('gvergaraa77', href='https://github.com/gvergaraa77', style={'textDecoration': 'none'},target='_blank'),
-                    ]),
-                    html.Li([
-                        html.Img(src='https://api.iconify.design/academicons/google-scholar.svg', style={'width': '5%', 'height': '5%'}),
-                        ' ',
-                        html.A('Gustavo Vergara',href='https://scholar.google.es/citations?hl=es&pli=1&user=qumtL4IAAAAJ', style={'textDecoration': 'none'},target='_blank'),
-                    ])
+                    dbc.Col([]),
+                ]),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([],lg=0.5),
+                    dbc.Col([
+                        html.Div([
+                            html.Ul([
+                                html.Li('Mathematician'),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/arcticons/huawei-email.svg', style={'width': '15px', 'height': '15px'}),
+                                    ' gustavovergara238@gmail.com']),
+                                html.Li([
+                                    html.Img(src="https://api.iconify.design/logos/linkedin-icon.svg", style={'width': '5%', 'height': '5%'}),  
+                                    ' ',                  
+                                    html.A('Gustavo Vergara', href='https://www.linkedin.com/in/gvergaraa77/', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/ant-design/github-filled.svg', style={'width': '5%', 'height': '5%'}),
+                                    ' ',                      
+                                    html.A('gvergaraa77', href='https://github.com/gvergaraa77', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                            ]),
+                        ]),
+                    ],lg=11),
+                    dbc.Col([],lg=0.5),
                 ]),
             ],style=style_div)    
         ]),
         dbc.Col([
             html.Div([
-
+                dbc.CardHeader(html.H4('Sergio Sosa', style={'textAlign': 'center'})),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([]),
+                    dbc.Col([
+                        html.Div([
+                            html.Img(src="assets/profile/ss.png",style={'width': '150px', 'height': '150px','border-radius':'50%'}),
+                        ], style={'textAlign': 'center'}),
+                    ]),
+                    dbc.Col([]),
+                ]),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([],lg=0.5),
+                    dbc.Col([
+                        html.Div([
+                            html.Ul([
+                                html.Li('Economist'),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/arcticons/huawei-email.svg', style={'width': '15px', 'height': '15px'}),
+                                    ' sergioasb8@gmail.com',]),
+                                html.Li([
+                                    html.Img(src="https://api.iconify.design/logos/linkedin-icon.svg", style={'width': '5%', 'height': '5%'}),
+                                    ' ',
+                                    html.A('Sergio Sosa Bautista', href='https://www.linkedin.com/in/sergioasb8/', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/ant-design/github-filled.svg', style={'width': '5%', 'height': '5%'}),
+                                    ' ',
+                                    html.A('sergioasb8', href='https://github.com/sergioasb8', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                            ]),
+                        ]),
+                    ],lg=11),
+                    dbc.Col([],lg=0.5),
+                ]),
             ], style=style_div),
         ]),
         dbc.Col([
             html.Div([
-
+                dbc.CardHeader(html.H4('Gustavo Vergara', style={'textAlign': 'center'})),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([]),
+                    dbc.Col([
+                        html.Div([
+                            html.Img(src="assets/profile/gv.png",style={'width': '150px', 'height': '150px','border-radius':'50%'}),    
+                        ], style={'textAlign': 'center'}),
+                    ]),
+                    dbc.Col([]),
+                ]),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([],lg=0.5),
+                    dbc.Col([
+                        html.Div([
+                            html.Ul([
+                                html.Li('Mathematician'),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/arcticons/huawei-email.svg', style={'width': '15px', 'height': '15px'}),
+                                    ' gustavovergara238@gmail.com']),
+                                html.Li([
+                                    html.Img(src="https://api.iconify.design/logos/linkedin-icon.svg", style={'width': '5%', 'height': '5%'}),  
+                                    ' ',                  
+                                    html.A('Gustavo Vergara', href='https://www.linkedin.com/in/gustavo-antonio-vergara-rolong-22b15a144/', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/ant-design/github-filled.svg', style={'width': '5%', 'height': '5%'}),
+                                    ' ',                      
+                                    html.A('gvergaraa77', href='https://github.com/gvergaraa77', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/academicons/google-scholar.svg', style={'width': '5%', 'height': '5%'}),
+                                    ' ',
+                                    html.A('Gustavo Vergara',href='https://scholar.google.es/citations?hl=es&pli=1&user=qumtL4IAAAAJ', style={'textDecoration': 'none'},target='_blank'),
+                                ])
+                            ]),
+                        ]),
+                    ],lg=11),
+                    dbc.Col([],lg=0.5),
+                ]),
             ], style=style_div),
         ]),
     ]),
@@ -66,13 +155,83 @@ layout=html.Div([
     dbc.Row([
         dbc.Col([
             html.Div([
-
-            ], style=style_div),
+                dbc.CardHeader(html.H4('Wilson Caro', style={'textAlign': 'center'})),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([]),
+                    dbc.Col([
+                        html.Div([
+                            html.Img(src="assets/profile/gv.png",style={'width': '150px', 'height': '150px','border-radius':'50%'}),    
+                        ], style={'textAlign': 'center'}),
+                    ]),
+                    dbc.Col([]),
+                ]),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([],lg=0.5),
+                    dbc.Col([
+                        html.Div([
+                            html.Ul([
+                                html.Li('Matematician'),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/arcticons/huawei-email.svg', style={'width': '15px', 'height': '15px'}),
+                                    ' gustavovergara238@gmail.com']),
+                                html.Li([
+                                    html.Img(src="https://api.iconify.design/logos/linkedin-icon.svg", style={'width': '5%', 'height': '5%'}),  
+                                    ' ',                  
+                                    html.A('Gustavo Vergara', href='https://www.linkedin.com/in/gustavo-antonio-vergara-rolong-22b15a144/', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/ant-design/github-filled.svg', style={'width': '5%', 'height': '5%'}),
+                                    ' ',                      
+                                    html.A('gvergaraa77', href='https://github.com/gvergaraa77', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                            ]),
+                        ]),
+                    ],lg=11),
+                    dbc.Col([],lg=0.5),
+                ]),
+        ], style=style_div),
         ]),
         dbc.Col([
             html.Div([
-
-            ], style=style_div),
+                dbc.CardHeader(html.H4('Juan Arteaga', style={'textAlign': 'center'})),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([]),
+                    dbc.Col([
+                        html.Div([
+                            html.Img(src="assets/profile/jf.png",style={'width': '150px', 'height': '150px','border-radius':'50%'}),    
+                        ], style={'textAlign': 'center'}),
+                    ]),
+                    dbc.Col([]),
+                ]),
+                html.Br(),
+                dbc.Row([
+                    dbc.Col([],lg=0.5),
+                    dbc.Col([
+                        html.Div([
+                            html.Ul([
+                                html.Li('Mechanical Engineer'),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/arcticons/huawei-email.svg', style={'width': '15px', 'height': '15px'}),
+                                    ' gustavovergara238@gmail.com']),
+                                html.Li([
+                                    html.Img(src="https://api.iconify.design/logos/linkedin-icon.svg", style={'width': '5%', 'height': '5%'}),  
+                                    ' ',                  
+                                    html.A('Juan Felipe Arteaga', href='https://www.linkedin.com/in/felipe-arteaga-r/', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                                html.Li([
+                                    html.Img(src='https://api.iconify.design/ant-design/github-filled.svg', style={'width': '5%', 'height': '5%'}),
+                                    ' ',                      
+                                    html.A('FelipeArteaga', href='https://github.com/FelipeArteaga', style={'textDecoration': 'none'},target='_blank'),
+                                ]),
+                            ]),
+                        ]),
+                    ],lg=11),
+                    dbc.Col([],lg=0.5),
+                ]),
+           ], style=style_div),
         ]),
         dbc.Col([]),
     ]),
